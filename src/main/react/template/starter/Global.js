391d5144ab43704891c051004f6dc9572293ce2a
John <john@starter.io>
2018-05-29 20:18:01 -0700

    removing obsolete hosts

--- src/main/react/template/starter/Global.js
+++ src/main/react/template/starter/Global.js
@@ -12,8 +12,6 @@ if(typeof global.self === "undefined")
     MAJOR_VERSION : '.1',
     MINOR_VERSION : '0.1.27',
     API_HOST: 'https://api.{{appname}}.online/',
-    //API_HOST : 'http://192.168.2.116:8080/',
-  //  API_HOST : 'http://localhost:8080/',
     API_VERSION : '1.0',
     COLOR: {
       ORANGE: '#FF9900',

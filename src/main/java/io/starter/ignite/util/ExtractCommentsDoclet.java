62b9b1d7cde65ac517ff5e1005b17ca62f92676a
John McMahon <john@starter.io>
2018-06-25 07:17:39 -0700

    adding extract comments doclet

---
+++ src/main/java/io/starter/ignite/util/ExtractCommentsDoclet.java
@@ -0,0 +1,35 @@
+package io.starter.ignite.util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ExtractCommentsDoclet {
+
+	public static boolean start(RootDoc root) throws IOException {
+		for (ClassDoc c : root.classes()) {
+			print(c.qualifiedName(), c.commentText());
+			for (FieldDoc f : c.fields(false)) {
+				print(f.qualifiedName(), f.commentText());
+			}
+			for (MethodDoc m : c.methods(false)) {
+				print(m.qualifiedName(), m.commentText());
+				if (m.commentText() != null && m.commentText().length() > 0) {
+					for (ParamTag p : m.paramTags())
+						print(m.qualifiedName() + "@" + p.parameterName(), p.parameterComment());
+					for (Tag t : m.tags("return")) {
+						if (t.text() != null && t.text().length() > 0)
+							print(m.qualifiedName() + "@return", t.text());
+					}
+				}
+			}
+		}
+		return true;
+	}
+
+	private static void print(String name, String comment) throws IOException {
+		if (comment != null && comment.length() > 0) {
+			comment = "<html><body><h1>" + comment + "</h1></body></html>";
+			new FileWriter(name + ".html").append(comment).close();
+		}
+	}
+}

989707c2ae6d4582008bbbb1e4c00555b9e562f8
John McMahon <john@starter.io>
2020-03-14 10:19:51 -0700

    feat(core): cleanup SNAFU

--- src/test/java/io/starter/ignite/generator/JavaGenTest.java
+++ src/test/java/io/starter/ignite/generator/JavaGenTest.java
@@ -13,16 +13,9 @@ public class JavaGenTest {
 
 		final Class<? extends User> uc = io.starter.stackgentest.model.User.class;
 
-		final Object o = Gen.getApiModelPropertyAnnotation(uc.getField("userName"));
+		final Object o = Gen.getApiModelPropertyAnnotation(uc.getField("governmentId"));
 		Assert.assertEquals(o.toString(),
-				"@io.swagger.annotations.ApiModelProperty(dataField=\"\", "
-						+ "secureField=false, access=\"\", notes=\"\", hidden=false, "
-						+ "maxValue=1.7976931348623157E308, minLength=0, dataType=\"\","
-						+ " readOnly=false, accessMode=AUTO, required=false, example=\"Sparky\", "
-						+ "reference=\"\", extensions={@io.swagger.annotations.Extension(name=\"\", "
-						+ "properties={@io.swagger.annotations.ExtensionProperty(name=\"\", value=\"\")})}, "
-						+ "minValue=4.9E-324, allowableValues=\"\", allowEmptyValue=false, name=\"\", position=0, "
-						+ "value=\"\", maxLength=256)");
+				"@io.swagger.annotations.ApiModelProperty(dataField=, secureField=false, access=, notes=, hidden=false, maxValue=1.7976931348623157E308, dataType=, minLength=10, readOnly=false, accessMode=AUTO, required=false, example=1112233334, reference=, extensions=[@io.swagger.annotations.Extension(name=, properties=[@io.swagger.annotations.ExtensionProperty(name=, value=)])], minValue=4.9E-324, allowableValues=, allowEmptyValue=false, name=, position=0, value=a 10 digit government ID (encrypted), maxLength=10)");
 	}
 
 	@Test
